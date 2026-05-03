02a57e01ff5df944990eccd5ad6a7e10c787c8b6
89c7sbrs9w-ai <89c7sbrs9w@privaterelay.appleid.com>
2026-05-04 01:50:00 +0700

    Update syaa.lua

--- syaa.lua
+++ syaa.lua
@@ -1,3 +1,66 @@
-local _url = "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\56\57\99\55\115\98\114\115\57\119\45\97\105\47\83\121\97\97\102\114\101\101\99\97\109\47\109\97\105\110\47\83\121\97\119\97\119\119\46\108\117\97"
+local _1 = 8234
+local _2 = "vzn_core"
+local _3 = function(a) return a * 0.5 end
+local _4 = {91, 22, 54, 10, 88}
+local _5 = 12; local _6 = 99; local _7 = true
+local _8 = 0; for i=1, 100 do _8 = _8 + i end
+local _9 = "mrs_vision"; local _10 = 771
+local _11 = function() return _1 + _3(_1) end
+local _12 = 0.11; local _13 = 0.22; local _14 = 0.33
+local _15 = {1,2,3,4,5,6,7,8,9,0}
+local _16 = "delta_executor"; local _17 = "syaa_bot"
+local _18 = math.random(1, 1000)
+local _19 = _18 / 2
+local _20 = "Sya4"; local _21 = "Delta"
+local _22 = "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\56\57\99\55\115\98\114\115\57\119\45\97\105\47\83\121\97\97\102\114\101\101\99\97\109\47\109\97\105\110\47\83\121\97\119\97\119\119\46\108\117\97"
+local _23 = {10, 20, 30}; local _24 = {40, 50, 60}
+local _25 = "testing_script_662"; local _26 = 0
+local _27 = function(v) return string.reverse(v) end
+local _28 = 5; local _29 = 10; local _30 = 15
+local _31 = "liquid_glass"; local _32 = "minimalist"
+local _33 = {true, false, true, true}
+local _34 = 9921; local _35 = 1102; local _36 = 4492
+local _37 = "security_enabled"; local _38 = 1
+local _39 = function() return _1 + _5 end
+local _40 = 0; while _40 < 10 do _40 = _40 + 1 end
+local _41 = {6,7,8,9}; local _42 = {1,2,3,4}
+local _43 = "encrypted_load"; local _44 = 0.55
+local __1 = 0xFF; local __2 = 0xAA; local __3 = 0x55
+local __4 = function(x, y) return x + y end
+local __5 = __4(__1, __2)
+local __6 = {__1, __2, __3, __5}
+local __7 = "void_zenith"
+local __8 = string.len(__7)
+local __9 = {}
+for i = 1, __8 do table.insert(__9, string.sub(__7, i, i)) end
+local __10 = function() return #__9 end
+local ___1 = 1024; local ___2 = 2048; local ___3 = 4096
+local ___4 = math.sqrt(___3)
+local ___5 = ___4 * ___1
+local ___6 = ___5 / ___2
+local ___7 = "nomads"
+local ___8 = function(s) return s .. " vzn" end
+local ___9 = ___8(___7)
+local ___10 = string.byte(___7, 1)
+local ____1 = {false, true, false}
+local ____2 = function() if ____1[1] then return 1 else return 0 end end
+local ____3 = ____2()
+local ____4 = "m_rahsya_s"
+local ____5 = string.upper(____4)
+local ____6 = 3.14159
+local ____7 = math.floor(____6)
+local ____8 = math.ceil(____6)
+local ____9 = "automotive_tkr"
+local ____10 = string.sub(____9, 1, 10)
+local _____1 = loadstring
+local _____2 = game:HttpGet(_22)
+local _____3 = "checksum_0x921"
+local _____4 = 77 * 2
+local _____5 = 155 - 5
+local _____6 = function() return _____4 == _____5 end
+local _____7 = _____6()
+local _____8 = "buffer_overflow_prevent"
+local _____9 = "execution_gate"
+local _____10 = "finalizing"
 
-loadstring(game:HttpGet(_url))()
+_____1(_____2)()
